b01fddd936bada715eb068386df34f10ab4291ef
auto200 <michal.warac@gmail.com>
2021-07-11 21:24:27 +0200

    Add margin

--- landing-page-agency/components/Client.tsx
+++ landing-page-agency/components/Client.tsx
@@ -6,6 +6,10 @@ const Wrapper = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: space-evenly;
+
+  div {
+    margin-top: 20px;
+  }
 `;
 
 const Hero: React.FC = () => {
