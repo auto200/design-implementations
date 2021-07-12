f191d0d9378b5df3198139182142a83346b9c871
auto200 <michal.warac@gmail.com>
2021-07-12 13:12:10 +0200

    Make website a tiny bit responsive

--- landing-page-agency/components/sections/Hero.tsx
+++ landing-page-agency/components/sections/Hero.tsx
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import Image from "next/image";
-import background from "public/images/hero/background.png";
 import person from "public/images/hero/person.png";
 import PillButton from "components/shared/PillButton";
 
@@ -8,18 +7,15 @@ const Wrapper = styled.div`
   position: relative;
   height: 707px;
   border-radius: 35px;
+  background: url("images/hero/background.png");
   overflow: hidden;
   display: flex;
   color: ${({ theme }) => theme.colors.white};
   z-index: -1;
 `;
 
-const Background = styled.div`
-  position: absolute;
-  z-index: -1;
-`;
-
-const Text = styled.div`
+const Left = styled.div`
+  flex: 1;
   align-self: center;
   margin-left: 76px;
 
@@ -30,27 +26,25 @@ const Text = styled.div`
   }
 `;
 
-const PersonWrapper = styled.div`
+const Right = styled.div`
+  flex: 1;
   margin: auto 0 0 auto;
 `;
 
 const Hero: React.FC = () => {
   return (
     <Wrapper>
-      <Background>
-        <Image src={background} />
-      </Background>
-      <Text>
+      <Left>
         <h1>Go digital with us</h1>
         <p>
           We have designed hundreds of websites and helped improve their online
           performance through SEO, SEM and Social Media Marketing
         </p>
         <PillButton variant="light">See Portfolio</PillButton>
-      </Text>
-      <PersonWrapper>
+      </Left>
+      <Right>
         <Image src={person} />
-      </PersonWrapper>
+      </Right>
     </Wrapper>
   );
 };
