2b4e487b9cee09daf60d7c9a6681293170a970c4
auto200 <michal.warac@gmail.com>
2021-07-20 14:20:50 +0200

    Add tailwind css and starting files

--- components/pages/index.tsx
+++ components/pages/index.tsx
@@ -3,7 +3,8 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <Link href="single-price-grid">Single Price Grid</Link>
+      <Link href="/single-price-grid">Single Price Grid</Link>
+      <Link href="/stats-preview-card">Stats Preview Card</Link>
     </div>
   );
 }
---
+++ components/pages/stats-preview-card/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const StatsPreview = () => {
+  return (
+    <>
+      <Head>
+        <title>Stats Preview</title>
+      </Head>
+      <div className="my-3">text</div>
+    </>
+  );
+};
+
+export default StatsPreview;
