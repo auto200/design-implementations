da9b84d16ae63b4a87190baded63905f0c4234ee
auto200 <michal.warac@gmail.com>
2021-07-22 18:24:11 +0200

    Add colors and fonts

--- components/pages/stats-preview-card/index.tsx
+++ components/pages/stats-preview-card/index.tsx
@@ -5,6 +5,15 @@ const StatsPreview = () => {
     <>
       <Head>
         <title>Stats Preview</title>
+
+        {/* fonts */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        {/* @ts-ignore */}
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Lexend+Deca&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <div className="my-3">text</div>
     </>
