289f48a01f4af99825b15343a80db34d347a9a78
auto200 <michal.warac@gmail.com>
2021-07-22 18:26:33 +0200

    Mobile version

--- components/pages/stats-preview-card/index.tsx
+++ components/pages/stats-preview-card/index.tsx
@@ -1,4 +1,20 @@
+import { Fragment } from "react";
 import Head from "next/head";
+
+const stats = [
+  {
+    text: "10k+",
+    subText: "companies",
+  },
+  {
+    text: "314",
+    subText: "templates",
+  },
+  {
+    text: "12M+",
+    subText: "queries",
+  },
+];
 
 const StatsPreview = () => {
   return (
@@ -15,7 +31,36 @@ const StatsPreview = () => {
           rel="stylesheet"
         />
       </Head>
-      <div className="my-3">text</div>
+
+      <div className="bg-SPC-bg min-h-screen px-5 py-10 font-lexend-deca">
+        <div className="text-SPC-text-primary bg-SPC-card-bg rounded-md overflow-hidden text-center">
+          <div className="relative">
+            <img
+              src="/stats-preview-card/image-header-mobile.jpg"
+              className="filter"
+            />
+            <div className="absolute top-0 left-0 w-full h-full z-10 bg-SPC-accent opacity-60" />
+          </div>
+          <div className="px-5">
+            <div className="text-3xl mt-10 mb-5 font-inter font-bold">
+              Get <span className="text-SPC-accent">Insights</span> that help
+              your business grow.
+            </div>
+            <div className="text-SPC-text-secondary mb-10">
+              Discover the benefits of data analytics and make better decisions
+              regarding revenue, customer experience, and overall efficiency.
+            </div>
+            {stats.map(({ text, subText }) => (
+              <Fragment key={text}>
+                <h3 className="text-3xl font-inter font-bold">{text}</h3>
+                <p className="uppercase text-sm text-SPC-text-tertiary mb-7">
+                  {subText}
+                </p>
+              </Fragment>
+            ))}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
