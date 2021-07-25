d3166cdcfcff08d759138256064f024c38f5249f
auto200 <michal.warac@gmail.com>
2021-07-25 21:34:12 +0200

    Style homepage with links to components

--- components/pages/index.tsx
+++ components/pages/index.tsx
@@ -1,10 +1,38 @@
 import Link from "next/link";
+import Head from "next/head";
+
+const components = [
+  {
+    name: "Single Price Grid",
+    slug: "single-price-grid",
+  },
+  {
+    name: "Stats Preview Card",
+    slug: "stats-preview-card",
+  },
+];
 
 export default function Home() {
   return (
-    <div>
-      <Link href="/single-price-grid">Single Price Grid</Link>
-      <Link href="/stats-preview-card">Stats Preview Card</Link>
-    </div>
+    <>
+      <Head>
+        <title>Aggregation</title>
+      </Head>
+      <div className="min-h-screen bg-gray-700 text-gray-50">
+        <div className="text-center">
+          <h1 className="text-4xl">Components</h1>
+          <div className="flex flex-wrap text-center justify-center">
+            {components.map(({ name, slug }) => (
+              <div key={slug} className="w-96  m-10 cursor-pointer">
+                <h1 className="text-3xl font-bold">{name}</h1>
+                <Link href={`/${slug}`}>
+                  <img src={`/homepage/${slug}.jpg`} />
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
